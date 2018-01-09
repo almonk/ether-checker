28447146b110ad1f84658c3f3994b2ccc9b3b450
Glitch (spotted-saw) <none>
2018-01-09 19:31:07 +0000

    📙🎸 Checkpoint pages/test.vue:617779/195 package.json:617779/67

--- pages/test.vue
+++ pages/test.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     testetstest {{name}}
+    
+    {{title}}
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+  
 export default {
-  asyncData ({ req }) {
-    return {
-      name: req ? 'server' : 'client'
-    }
+  asyncData ({ params }, callback) {
+    axios.get(`https://api.coindesk.com/v1/bpi/currentprice.json`)
+    .then((res) => {
+      callback(null, { title: res.data.chartName })
+    })
   }
 }
 </script>
